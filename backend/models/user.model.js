facf75c1dfdecf65a7dd79f2a12a765abe50068c
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-04-07 16:13:53 -0700

    Defining user mongodb schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+//Defining user mongodb schema
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        profileImageUrl:{
+            type:String,
+            default: null
+        }
+    },
+    role:{
+        type: String,
+        enum: ['admin', 'user'],
+        default: "user"
+    }
+}, 
+{timestamps: true}
+);
+
+module.exports = mongoose.model('User', UserSchema)
