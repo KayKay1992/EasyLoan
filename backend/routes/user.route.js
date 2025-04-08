eb6089a197ec278ded8beeffc3f9bc28465b1cd3
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-04-08 10:18:04 -0700

    Integrating user route.

---
+++ backend/routes/user.route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+// const {
+//   getAllUsers,
+//   getUserById,
+//   updateUser,
+//   deleteUser,
+// } = require('../controllers/userController');
+
+const { protect, adminOnly } = require('../middleware/authMiddleware');
+
+
+
+
+// Admin routes And Private
+router.get('/', protect, adminOnly, getAllUsers); // Get all users
+router.get('/:id', protect, getUserById); // Get user by ID
+router.put('/:id', protect, adminOnly, updateUser); // Update user by ID
+router.delete('/:id', protect, adminOnly, deleteUser); // Delete user by ID
+
+module.exports = router;
