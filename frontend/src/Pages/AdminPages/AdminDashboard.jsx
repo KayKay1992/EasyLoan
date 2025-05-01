cdb52b87267fbe6f29178235966e0e43e46835c4
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-01 03:02:54 -0700

    API call to fetch dashboard Data.

--- frontend/src/Pages/AdminPages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminPages/AdminDashboard.jsx
@@ -1,12 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../context/userContext'
 import { useUserAuth } from '../../hooks/useUserAuth'
 import DashboardLayout from '../../Components/layouts/DashboardLayout'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
 
 const AdminDashboard = () => {
   useUserAuth()
 
   const {user} = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] =useState([]);
+  const [barChartData, setBarChartData] = useState([])
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.LOANS.GET_DASHBOARD_DATA
+      );
+      if(response.data){
+        setDashboardData(response.data)
+      }
+    }catch(error){
+      console.error('Error fetching users', error)
+    }
+  };
+
+  useEffect(()=> {
+    getDashboardData();
+    return () => { }
+  }, [])
+  
   return (
     <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>
   )
