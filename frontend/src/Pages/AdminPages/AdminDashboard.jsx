ae416d5865400d30bcb78c65d17b48cf78182661
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-01 12:04:22 -0700

    Admin Dashboard Summary section

--- frontend/src/Pages/AdminPages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminPages/AdminDashboard.jsx
@@ -5,6 +5,9 @@ import DashboardLayout from '../../Components/layouts/DashboardLayout'
 import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPaths'
+import { addThousandsSeparator } from '../../utils/helper'
+import moment from 'moment'
+import InfoCard from '../../Components/Cards/InfoCard'
 
 const AdminDashboard = () => {
   useUserAuth()
@@ -36,7 +39,57 @@ const AdminDashboard = () => {
   }, [])
   
   return (
-    <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>
+    <DashboardLayout activeMenu="Dashboard">
+            <div className="card my-3">
+        <div className="">
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">
+              Good Day! {user?.name}
+            </h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format('dddd Do MMM YYYY')}
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 md:gap-6 ">
+          <InfoCard
+             label='Total Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.All || 0)}
+             color='bg-primary'
+             />
+          <InfoCard
+             label='Active Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.active || 0)}
+             color='bg-violet-500'
+             />
+          <InfoCard
+             label='Approved Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.approved || 0)}
+             color='bg-cyan-500'
+             />
+          <InfoCard
+             label='Completed Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.completed || 0)}
+             color='bg-lime-500'
+             />
+          <InfoCard
+             label='Defaulted Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.defaulted || 0)}
+             color='bg-red-500'
+             />
+          <InfoCard
+             label='Pending Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.pending || 0)}
+             color='bg-amber-700'
+             />
+          <InfoCard
+             label='Rejected Loans'
+             value={addThousandsSeparator(dashboardData?.charts?.loanDistribution?.rejected || 0)}
+             color='bg-amber-700'
+             />
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
