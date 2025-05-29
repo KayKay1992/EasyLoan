2111fef52314f2f14c4456fdabefa42631ecf501
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-29 13:54:04 -0700

    ..

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../server');
 console.log('Imported app:', app); // Debug to confirm server.js is loaded
-module.exports.handler = serverless(app);
+module.exports.handler = async (req, res) => {
+  res.status(200).json({ message: 'Test handler works!' });
+};
