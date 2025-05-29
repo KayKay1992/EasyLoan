c9d61630b36ea94afa043669c4fd27b1c97f872e
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-29 16:21:43 -0700

    dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(),
+    
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase to 1000 kB to reduce warnings
+  },
 })
